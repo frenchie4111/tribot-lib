083fa0a94118ba52a4db636cf4018b18b8df0690
Mike Lyons <mdl0394@gmail.com>
2016-01-16 01:12:14 -0800

    Added times to WaitAction

--- src/scripts/lib/action/WaitAction.java
+++ src/scripts/lib/action/WaitAction.java
@@ -8,20 +8,46 @@ import org.tribot.api.types.generic.Condition;
  */
 public class WaitAction extends Action {
     private Condition _condition;
-    private int _timeout;
+    private int _timeout_min;
+    private int _timeout_max;
 
-    public WaitAction( Condition condition, int timeout ) {
+    public WaitAction( Condition condition, int timeout_min, int timeout_max ) {
         this._condition = condition;
-        this._timeout = timeout;
+        this._timeout_min = timeout_min;
+        this._timeout_max = timeout_max;
     }
 
     public WaitAction( Condition condition ) {
-        this( condition, 1000 );
+        this( condition, 1000, 1000 );
     }
+
+    public WaitAction( Condition condition, int timeout ) {
+        this( condition, timeout, timeout );
+    }
+
+    public WaitAction( int timeout ) {
+        this( new Condition() {
+            @Override
+            public boolean active() {
+                return false;
+            }
+        }, timeout, timeout );
+    }
+
+    public WaitAction( int timeout_min, int timeout_max ) {
+        this( new Condition() {
+            @Override
+            public boolean active() {
+                return false;
+            }
+        }, timeout_min, timeout_max );
+    }
+
 
     @Override
     public void start() {
         super.start();
-        Timing.waitCondition( this._condition, this._timeout );
+        double timeout = this._timeout_min + ( ( this._timeout_max - this._timeout_min ) * Math.random() );
+        Timing.waitCondition( this._condition, ( long ) timeout );
     }
 }
