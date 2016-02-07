24c191cd51c13922936a5d703a5a0ea8b843f8bf
BuildTools <unconfigured@null.spigotmc.org>
2016-02-07 21:07:34 +0000

    Fixed spaghetti and re-jigged object handling.

--- src/scripts/lib/action/camera/TurnToTile.java
+++ src/scripts/lib/action/camera/TurnToTile.java
@@ -1,22 +1,30 @@
 package scripts.lib.action.camera;
 
+import java.awt.Point;
+
 import org.tribot.api.interfaces.Positionable;
 import org.tribot.api2007.Camera;
+import org.tribot.api2007.Projection;
+
 import scripts.lib.action.Action;
 
 /**
  * Created by mike on 2/7/2016.
  */
 public class TurnToTile extends Action {
-    private Positionable _tile;
+	private Positionable _tile;
 
-    public TurnToTile( Positionable tile ) {
-        this._tile = tile;
-    }
+	public TurnToTile(Positionable tile) {
+		this._tile = tile;
+	}
 
-    @Override
-    public boolean run() {
-        Camera.turnToTile( this._tile );
-        return true;
-    }
+	@Override
+	public boolean run() {
+		Point tilePoint = this._tile.getPosition().getHumanHoverPoint();
+		if (tilePoint != null && !Projection.isInViewport(tilePoint)) {
+			Camera.turnToTile(this._tile);
+			return true;
+		}
+		return false;
+	}
 }
--- src/scripts/lib/action/inventory/UseOn.java
+++ src/scripts/lib/action/inventory/UseOn.java
@@ -1,47 +1,52 @@
 package scripts.lib.action.inventory;
 
 import org.tribot.api.DynamicClicking;
+import org.tribot.api.General;
 import org.tribot.api2007.Camera;
 import org.tribot.api2007.Inventory;
 import org.tribot.api2007.Objects;
 import org.tribot.api2007.types.RSItem;
 import org.tribot.api2007.types.RSObject;
+
 import scripts.lib.action.Action;
 import scripts.lib.antiban.Antiban;
+import scripts.lib.object.ObjectHandler;
 
 /**
  * Created by mike on 1/21/2016.
  */
 public class UseOn extends Action {
-    private String _item_name;
-    private String _target_name;
+	private String _item_name;
+	private String _target_name;
 
-    public UseOn( String item_name, String target_name ) {
-        super();
-        this._item_name = item_name;
-        this._target_name = target_name;
-    }
+	public UseOn(String item_name, String target_name) {
+		super();
+		this._item_name = item_name;
+		this._target_name = target_name;
+	}
 
-    @Override
-    public boolean run() {
-        System.out.println( "Running UseOne" );
-        RSItem items[] = Inventory.find( this._item_name );
-        if( items.length < 1 ) return false;
-        RSItem target_item = items[ 0 ];
-        System.out.println( items[ 0 ] );
+	@Override
+	public boolean run() {
+		System.out.println("Running UseOne");
+		RSItem items[] = Inventory.find(this._item_name);
+		if (items.length < 1)
+			return false;
+		RSItem target_item = items[0];
+		System.out.println(items[0]);
 
-        RSObject _cooker_objects[] = Objects.find( 20, this._target_name );
-        if( _cooker_objects.length < 1 ) return false;
-        RSObject cooker_object = _cooker_objects[ 0 ];
+		RSObject cooker_object = ObjectHandler.findNearest(this._target_name);
+		if (cooker_object == null)
+			return false;
 
-        Camera.turnToTile( cooker_object );
-        Antiban.afterActionSleep( 100, 200 );
+		Camera.turnToTile(cooker_object);
+		Antiban.afterActionSleep(100, 200);
 
-//        if( !target_item.click( "Use" ) ) return false;
-        target_item.click( "Use" );
-        DynamicClicking.clickRSObject( cooker_object, this._item_name + " -> " + this._target_name );
-//        if( ! ) return false;
-        Antiban.afterActionSleep( 100, 200 );
-        return true;
-    }
+		// if( !target_item.click( "Use" ) ) return false;
+		target_item.click("Use");
+		ObjectHandler.interact(cooker_object, this._item_name + " -> "
+				+ this._target_name);
+		// if( ! ) return false;
+		Antiban.afterActionSleep(100, 200);
+		return true;
+	}
 }
